f5990b68791eb74994219ae104d2e6bd27bee68f
astral-keks <astralkeks@gmail.com>
2017-08-22 01:58:54 +0300

    - Proper binaries and nupkg versioning

--- build.ps1
+++ build.ps1
@@ -1,9 +1,16 @@
+$version = "0.1.2"
+
 $src = "$PSScriptRoot\src"
 $package = "$PSScriptRoot\package"
 $artifact = "$PSScriptRoot\artifact\AstralKeks.ChefSpoon"
+
+$manifest = "$artifact\AstralKeks.ChefSpoon.psd1"
 
 # Build binaries
 if (Test-Path $artifact) { Remove-Item $artifact -Recurse }
 if (Test-Path $package) { Remove-Item $package -Recurse }
 dotnet restore $src\Command\ChefSpoon.Command.csproj
-dotnet build $src\Command\ChefSpoon.Command.csproj --configuration Release -o $artifact
+dotnet build $src\Command\ChefSpoon.Command.csproj --configuration Release -o $artifact --version-suffix $version
+
+# Fix manifest version
+(Get-Content $manifest).replace("ModuleVersion = '0.0.1'", "ModuleVersion = '$version'") | Set-Content $manifest
